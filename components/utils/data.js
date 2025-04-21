b3c3b6c8262a9b808b92144ceb9d669f0911cd87
gbotemiwebagency <gbotemiwebagency@gmail.com>
2025-04-21 20:07:25 +0100

    advans events

---
+++ components/utils/data.js
@@ -0,0 +1,17 @@
+export const firstLetter = "A";
+export const companyName = "dvansEvents";
+export const shortName = "Advans"
+export const phoneNumber = "+2348065141043";
+export const companyAddress = "44 Community Rd, off Allen Avenue, Ikeja, Lagos 100128, Lagos";
+// images down here
+export const expertise1 = "../startup/advans-events-001.webp";
+export const expertise2 = "../startup/advans-events-002.webp";
+export const expertise3 = "../startup/advans-events-003.webp";
+export const expertise4 = "../startup/advans-events-004.webp";
+
+export const showcase1 = "../startup/advans-events-005.webp";
+export const showcase2 = "../startup/advans-events-006.webp";
+export const showcase3 = "../startup/advans-events-007.webp";
+export const showcase4 = "../startup/advans-events-008.webp";
+export const showcase5 = "../startup/advans-events-001.webp";
+export const showcase6 = "../startup/advans-events-002.webp";
